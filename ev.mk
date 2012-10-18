e26a6cf72e3cba90175c7b80c9b4a5c2728ab348
jmztaylor <jmztaylor@gmail.com>
2012-10-18 18:09:59 -0300

    fix typo

--- ev.mk
+++ ev.mk
@@ -19,7 +19,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/na
 PRODUCT_CODENAME := Effor
 PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
-PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Evo Shift 4G\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @jmz_software for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
+PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Evo View 4G\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @jmz_software for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
 # Hot reboot
 PRODUCT_PACKAGE_OVERLAYS += \
